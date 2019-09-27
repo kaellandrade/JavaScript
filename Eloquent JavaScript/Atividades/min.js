fa6ea3d1e3f0be4db7286e86b28f27f2a678c943
kaellandrade <micael.santos@dcomp.ufs.br>
2019-09-27 20:46:36 -0300

    add funcion.js menor

--- Eloquent JavaScript/Atividades/min.js
+++ Eloquent JavaScript/Atividades/min.js
@@ -1,3 +1,12 @@
 /*
 Escreva uma função min que recebe dois argumentos e retorna o menor deles.
 */
+
+function menor(x, y) {
+    if (x <= y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+console.log(menor(0, -10))
