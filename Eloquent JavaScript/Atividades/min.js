a8b1a38da92ba5cf7df81a1ffe61dbe133cca53a
kaellandrade <micael.santos@dcomp.ufs.br>
2019-09-27 22:19:16 -0300

    update

--- Eloquent JavaScript/Atividades/min.js
+++ Eloquent JavaScript/Atividades/min.js
@@ -9,5 +9,38 @@ function menor(x, y) {
         return y;
     }
 }
-console.log(menor(0, -10))
+// console.log(menor(0, -10))
 
+// É par usando recursão
+// positivo = Math.abs
+function isEven(n) {
+
+    if (n  == 0) {
+        return true; 
+    } else if ( n == 1 ){
+        return false;
+    } else if (n > 0){
+        return isEven(n-2)
+    } else{
+        return isEven(-n);
+    }
+}
+// console.log(isEven(-1))
+
+//Função que conta quantos char há na string.
+function countChar(str, char) {
+    var index = 0;
+    var cont = 0;
+    while (index <= str.length) {
+        if (str.charAt(index) == char) {
+            cont ++;
+            index ++;
+
+        } else {
+            index ++;            
+        }
+    }
+    return cont;
+}
+
+console.log(countChar("kakkerlak", "k"));
