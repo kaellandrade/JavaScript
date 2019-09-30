06c8d34ac25a2e331f8d6dd68069d07cc414666a
kaellandrade <micael.santos@dcomp.ufs.br>
2019-09-30 20:47:24 -0300

    update!

---
+++ CursoEmVideo/Aula-13/ambientetest.js
@@ -0,0 +1,28 @@
+// ----------------Usando While em JS
+var c = 1
+
+/*
+// Estrutura de repetição com teste lógico no final
+while (c <= 6) {
+    console.log(`${c}º tudo bem ?`)
+    c++;
+}
+*/
+
+// Estrutura de repetição com tesmte lógico no final 
+/*
+do {
+    console.log(`${c}º Passo.`)
+    c++
+} while (c <= 6);
+*/
+
+
+//------------------------Usando for em JS
+//for (inicio; teste; incremento){}
+
+console.log("Start!")
+for(let c=1; c <= 4 ; c++){
+    console.log(`${c}º passo.`)
+}
+console.log("The End!")
