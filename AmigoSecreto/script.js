e4ac3c8de3e538c55f3263ff2c4751180b27346f
Micael <micael.santos@dcomp.ufs.br>
2020-09-08 20:15:27 -0300

    update

--- AmigoSecreto/script.js
+++ AmigoSecreto/script.js
@@ -7,7 +7,7 @@ let tabela = document.querySelector('#tabala-nomes');
 let contagem = document.querySelector('#contagem');
 
 let todosOsAmigos = []; // array onde será armazenado todos os amigos cadastrados;
-let amigosJaSorteados = []; // armazena os amigos já sorteados
+let sorteados = []; // armazena os amigos já sorteados
 let pares_amigos = []; // pares dos amigos formados
 
 
@@ -39,11 +39,7 @@ addEventListener('keydown', enterAdd);
 
 // TODO : Ajustar o sorteio apenas para 3 pessoas ou mais.
 btnSortear.addEventListener('click', ()=>{
-    formarOsAmigos();
-    console.log(pares_amigos);
-    // Limpa as variáveis para tentar um novo sorteio
-    amigosJaSorteados = [];
-    pares_amigos = [];
+    console.log('Sorteando...')
 })
 
 function capitalize(nome) { // Nome maiúsculo
@@ -98,98 +94,3 @@ function enterAdd(e) {
         inputNome.value = '';
     }
  }
-
-
- // Script 
-function formarOsAmigos() { 
-    todosOsAmigos.forEach((item) => {
-        pares_amigos.push([item, sortearAmigo(item)]);
-    })
- }
-
-
-function sortearAmigo(amigo){
-    do {
-        var numSorteado = Math.floor(Math.random() * todosOsAmigos.length);
-        var amigoSorteado = todosOsAmigos[numSorteado];
-
-    } while (todosOsAmigos[numSorteado] == amigo || amigosJaSorteados.includes(numSorteado));
-    amigosJaSorteados.push(numSorteado); // guarda o número sorteado
-    return amigoSorteado;
-    
-}
-
-/*
-var listaNomes = [] 
-
-var res = document.getElementById('resposta')
-var n = document.querySelector('input#nome')
-var sel = document.getElementById('mylist')
-
-// Função que adiciona nomes ao array
-function sortearAmigo() {
-    if(listaNomes.length > 0){
-        var amigo = random(listaNomes)
-        res.innerHTML = amigo.toUpperCase()
-        let item = sel.getElementsByTagName('li');
-        sel.removeChild(item[listaNomes.indexOf(amigo)]) // AQUI!
-        removepessoa(listaNomes, amigo);
-
-    } else{
-        alert("Adicione amigos primeiro!")
-    }
-    
-}
-
-function addValue() {
-    
-    let nome = n.value.trim().toLowerCase() // Deixa o nome em lower e sem espaço no início!
-    res.innerHTML = ''
-    if(nome.length == 0){
-        alert("Nome inválido!")
-    }
-    else if (listaNomes.indexOf(nome) != -1) { // Se o nome não estiver na lista será retonrado -1
-        alert(`${nome} já está na lista!`) 
-        
-    } else{
-        listaNomes.push(nome)
-        let node = document.createElement('li')
-        var textnode = document.createTextNode(`${title(nome)}`) 
-        node.appendChild(textnode)
-        document.getElementById("mylist").appendChild(node)
-    }
-    n.value = '' // Limpado campo
-    n.focus() // Deixa o foco em n 
-}
-
-// limpa a lista
-function limpar(){
-    resposta = confirm("Deseja apagar todos os amigos cadastrados?");
-    if(resposta){
-        listaNomes = [];
-        sel.innerHTML = ''
-        res.innerHTML = ''
-    }
-    
-}
-
-// Deixa a primeira letra maúscula Ex: Mikael
-function title(palavra) {
-    name = palavra[0].toUpperCase() + palavra.slice(1).toLowerCase()
-    return name
-}
-
-function random(array){
-    var item = array[Math.floor(Math.random() * array.length)]
-    return item
-}
-
-// Remove um lemento
-function removepessoa(array, name) {
-    var index = array.indexOf(name);
-    if (index > -1){
-        var name = array.splice(index, 1)
-    }
-    return name[0]
-}
-*/
